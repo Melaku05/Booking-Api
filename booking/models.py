de13d4300dbdeefd70aad80c2e5329a428ab0793
melaku05 <melakushiferaw05@gmail.com>
2022-09-20 10:01:31 +0300

    Create reservation model

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from core.models import User
 
 # Create your models here.
 class Doctor(models.Model):
@@ -12,3 +13,14 @@ class Doctor(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
+
+class Reservation(models.Model):
+    city= models.CharField(max_length=100)
+    date = models.DateTimeField()
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.user
+
