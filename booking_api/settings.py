62970797b489c3eb9a11e85dd0a417cb78d247f5
melaku05 <melakushiferaw05@gmail.com>
2022-09-22 08:25:03 +0300

    test 1

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j$k25&*$gb%k$e0@13@d+aoa+o@6!s2v@eu4i-n_(kho)@4$xa'
+#SECRET_KEY = 'django-insecure-j$k25&*$gb%k$e0@13@d+aoa+o@6!s2v@eu4i-n_(kho)@4$xa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
